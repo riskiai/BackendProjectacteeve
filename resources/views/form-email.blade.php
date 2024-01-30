92bd83689ae980260d5bda68d1f44d97842f4f16
riskiai <riskiahmadilham2002@gmail.com>
2024-01-30 12:50:41 +0700

    menambahakan form email untuk percobaan data

---
+++ resources/views/form-email.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesan Masuk</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+    <div class="container">
+        <header>
+            <h1>PT ACTEEVE</h1>
+        </header>
+        <section>
+            <h3>Hallo, Riski Ahmad Ilham</h3>
+            <p ><strong>Selamat <br> Registrasi akun anda Telah Berhasil.</strong></p>
+            <p style="text-align:center;"><strong>Email : riskiahmadiilham@gmail.com</strong></p>
+            <p style="text-align:center;" ><strong>Password : 1234567</strong></p>
+            <p><strong>Terimakasih <br> Sudah mengguanakan layanan kami. <br> <br>
+            <h3 style="color: white;">Hormat Kami <br><br> PT ACTEEVE</h3>
+            {{-- <p><strong>Name:</strong> {{ $mailData['name'] }}</p>
+            <p><strong>Email:</strong> {{ $mailData['email'] }}</p>
+            <p><strong>Phone:</strong> {{ $mailData['phone'] }}</p>
+            <p><strong>Message:</strong> {{ $mailData['message'] }}</p> --}}
+        </section>
+        <footer>
+            <p>Terima kasih</p>
+        </footer>
+    </div>
+
+</body>
+</html>
