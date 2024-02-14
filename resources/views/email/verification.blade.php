8da4ed09c4299da936afa593305265640fb817e8
riskiai <riskiahmadilham2002@gmail.com>
2024-02-15 00:50:58 +0700

    penamabahan style bacground pada email

--- resources/views/email/verification.blade.php
+++ resources/views/email/verification.blade.php
@@ -6,7 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pesan Masuk</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+
+    <style>
+        body {
+            background-color: grey;
+        }
+    </style>
 </head>
 
 <body>
