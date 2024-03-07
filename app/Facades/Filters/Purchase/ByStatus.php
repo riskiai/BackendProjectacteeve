f95b86081da125467336b24f56cddd1ea8a2943c
hakim-asrori <saneglos005@gmail.com>
2024-03-08 00:00:44 +0700

    filtering status

--- app/Facades/Filters/Purchase/ByStatus.php
+++ app/Facades/Filters/Purchase/ByStatus.php
@@ -2,6 +2,9 @@
 
 namespace App\Facades\Filters\Purchase;
 
+use App\Models\Purchase;
+use App\Models\PurchaseStatus;
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -13,8 +16,19 @@ class ByStatus
             return $next($query);
         }
 
-        $query->where('purchase_status_id', request('status', 1));
-
+        if (in_array(request('status'), [PurchaseStatus::AWAITING, PurchaseStatus::REJECTED, PurchaseStatus::PAID])) {
+            $query->where('purchase_status_id', request('status'));
+        } else {
+            $now = Carbon::now();
+            $query->whereNotIn('purchase_status_id', [PurchaseStatus::AWAITING, PurchaseStatus::REJECTED, PurchaseStatus::PAID]);
+            if (request('status') == PurchaseStatus::OPEN) {
+                $query->where('due_date', '>', $now->toDateString());
+            } elseif (request('status') == PurchaseStatus::OVERDUE) {
+                $query->where('due_date', '<', $now->toDateString());
+            } else {
+                $query->whereDate('due_date', $now->toDateString());
+            }
+        }
         return $next($query);
     }
 }
