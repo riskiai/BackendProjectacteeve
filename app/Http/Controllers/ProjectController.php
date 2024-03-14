c1f6d279b58127f59928e9907ec0b3da25bc761f
riskiai <riskiahmadiilham@gmail.com>
2024-03-14 15:45:06 +0700

    Update

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -33,21 +33,18 @@ class ProjectController extends Controller
 
         $query->where(function ($query) {
             if (auth()->user()->role_id == Role::USER) {
-                // Jika pengguna adalah 'USER', tampilkan semua proyek yang aktif di pembelian dan proyek yang dibuat oleh pengguna saat ini
+                // Jika pengguna adalah 'USER', tampilkan semua proyek yang aktif di pembelian
                 $query->whereHas('purchases.project', function ($query) {
-                    $query->where('status', Project::ACTIVE)
-                          ->whereHas('user', function ($query) {
-                              $query->where('id', auth()->user()->id);
-                          });
+                    $query->where('status', Project::ACTIVE);
                 });
             } else {
                 // Jika bukan pengguna biasa, tampilkan semua proyek
                 $query->whereNotNull('id');
             }
         });
+             
         
-        
-        
+
         if ($request->has('search')) {
             $query->where(function ($query) use ($request) {
                 $query->where('id', 'like', '%' . $request->search . '%');
